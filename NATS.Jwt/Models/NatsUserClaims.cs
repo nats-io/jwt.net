848530cbe57922137af6d4cffe0243ba072f3afa
Chuck Kasabula <ckasabula@gmail.com>
2024-08-14 10:30:00 -0400

    Adds NatsJwt.SetScopedUserClaims (#6)
    https://github.com/nats-io/jwt/blob/41dad79143e9cff55e4bde8af4df7a40ad49786c/v2/user_claims.go#L78
    
    Co-authored-by: Chuck Kasabula <charles.kasabula@amag.com>

--- NATS.Jwt/Models/NatsUserClaims.cs
+++ NATS.Jwt/Models/NatsUserClaims.cs
@@ -17,4 +17,35 @@ public record NatsUserClaims : JwtClaimsData
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
     [JsonPropertyOrder(1)]
     public NatsUser User { get; set; } = new();
+
+    /// <summary>
+    /// Sets the user claims as scoped or not.
+    /// </summary>
+    /// <param name="scoped">Indicates whether the user claims should be scoped or not.</param>
+    public void SetScoped(bool scoped)
+    {
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+        User.Src = default;
+        User.Times = default;
+        User.Locale = default;
+
+        if (scoped)
+        {
+            User.Pub = new NatsPermission();
+            User.Sub = new NatsPermission();
+            User.Resp = default;
+            User.Subs = 0;
+            User.Data = 0;
+            User.Payload = 0;
+            User.BearerToken = default;
+            User.AllowedConnectionTypes = default;
+        }
+        else
+        {
+            User.Subs = NatsJwt.NoLimit;
+            User.Data = NatsJwt.NoLimit;
+            User.Payload = NatsJwt.NoLimit;
+        }
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+    }
 }
